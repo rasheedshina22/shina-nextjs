e6ad987ebde86d4d362af2fd3847ee568e36d665
shina <rasheedshinaopeyemi@gmail.com>
2021-02-06 16:19:06 +0100

    new changes to the about page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function about() {
   return (
     <div style={{ marginBottom: '5rem' }}>
-      this is the about page
+      <h5> this is the about page</h5>
       <Link href="/">Go back</Link>
     </div>
   );
